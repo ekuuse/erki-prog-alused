0c99b85d86783425ed670eb610efb57b92c3aec4
ekuuse <erki.kuuse@voco.ee>
2024-09-09 10:45:33 +0300

    read data from file

--- schoolstuff/todoweb/index.js
+++ schoolstuff/todoweb/index.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
 
 const path = require('path')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
-    res.render('index')
+    fs.readFile('./tasks', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        const tasks = data.split("\n")
+        res.render('index', {tasks : tasks})
+    })
+})
+
+app.get('/other', (req, res) => {
+    res.render('other')
 })
 
 app.listen(3001, () => {
-    console.log('localhost://3001')
+    console.log('http://localhost:3001')
 })
