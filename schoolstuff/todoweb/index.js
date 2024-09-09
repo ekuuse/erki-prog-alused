1476fb75af41fe2ba15215c8992a1fe2b513d121
ekuuse <erki.kuuse@voco.ee>
2024-09-09 11:50:12 +0300

    deleted files not finished

--- schoolstuff/todoweb/index.js
+++ schoolstuff/todoweb/index.js
@@ -5,6 +5,7 @@ const fs = require('fs')
 const path = require('path')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+app.use(express.urlencoded({extended : true}))
 
 const readFile = (filename) => {
     return new Promise((resolve, reject) => {
@@ -13,16 +14,45 @@ const readFile = (filename) => {
                 console.error(err)
                 return
             }
-            const tasks = data.split("\n")
+            const tasks = JSON.parse(data)
             resolve(tasks)
         })
     })
 }
 
+const writeFile = (filename, data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(filename, data, 'utf-8', err => {
+            if (err) {
+                console.error(err)
+                return
+            }
+            resolve(true)
+        })
+    })
+}
+
 app.get('/', (req, res) => {
-    readFile('./tasks')
+    readFile('./tasks.json')
     .then(tasks =>{
         res.render('index', {tasks: tasks})
+    })
+})
+
+
+app.get('/delete-task/:taskId', (req, res) => {
+    let deletedTaskId = req.params.taskId
+    readFile('./tasks.json')
+    .then(tasks => {
+        tasks.forEach((task, index) => {
+            if(task.id === deletedTaskId){
+                tasks.splice(index, 1)
+            }
+        })
+        data = JSON.stringify(tasks, null, 2)
+        writeFile('./tasks.json', data)
+        console.log('deleted')
+        res.redirect('/')
     })
 })
 
@@ -30,21 +60,26 @@ app.get('/other', (req, res) => {
     res.render('other')
 })
 
-app.use(express.urlencoded({extended : true}))
-
 app.post('/', (req, res) => {
     console.log(req.body)
-    readFile('./tasks')
+    readFile('./tasks.json')
     .then(tasks =>{
-        tasks.push(req.body.task)
-        const data = tasks.join("\n")
-        fs.writeFile('./tasks', data, err => {
-            if (err) {
-                console.error(err)
-                return
-            }
-            res.redirect('/')
-        })
+        let index
+        if(tasks.length === 0)
+        {
+            index = 0
+        } else {
+            index = tasks[tasks.length-1].id + 1;
+        }
+        
+        const newTask = {
+            "id" : index,
+            "task" : req.body.task
+        }
+        tasks.push(newTask)
+        data = JSON.stringify(tasks, null, 2)
+        writeFile('./tasks.json', data)
+        res.redirect('/')
     })
 })
 
