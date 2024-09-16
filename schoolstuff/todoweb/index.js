97842b8a94fb732dc68826c229bebcc739d16154
ekuuse <erki.kuuse@voco.ee>
2024-09-16 09:25:42 +0300

    error handling and deleting fixed

--- schoolstuff/todoweb/index.js
+++ schoolstuff/todoweb/index.js
@@ -35,17 +35,18 @@ const writeFile = (filename, data) => {
 app.get('/', (req, res) => {
     readFile('./tasks.json')
     .then(tasks =>{
-        res.render('index', {tasks: tasks})
+        res.render('index', {tasks: tasks, error: null})
     })
 })
 
 
 app.get('/delete-task/:taskId', (req, res) => {
-    let deletedTaskId = req.params.taskId
+    let deletedTaskId = parseInt(req.params.taskId)
     readFile('./tasks.json')
     .then(tasks => {
         tasks.forEach((task, index) => {
             if(task.id === deletedTaskId){
+                console.log('found task and deleted it')
                 tasks.splice(index, 1)
             }
         })
@@ -60,27 +61,47 @@ app.get('/other', (req, res) => {
     res.render('other')
 })
 
+function isValidString(input) {
+    return typeof input === 'string' && input.trim().length > 0;
+}
+
 app.post('/', (req, res) => {
-    console.log(req.body)
-    readFile('./tasks.json')
-    .then(tasks =>{
-        let index
-        if(tasks.length === 0)
-        {
-            index = 0
-        } else {
-            index = tasks[tasks.length-1].id + 1;
-        }
-        
-        const newTask = {
-            "id" : index,
-            "task" : req.body.task
-        }
-        tasks.push(newTask)
-        data = JSON.stringify(tasks, null, 2)
-        writeFile('./tasks.json', data)
-        res.redirect('/')
-    })
+    let error = null
+    if (!isValidString(req.body.task)) {
+        error = "Refused empty string or value that was not a string. Please enter a valid value."
+        readFile('./tasks.json')
+        .then(tasks =>{
+            res.render('index', {
+                tasks: tasks,
+                error: error
+            })
+        })
+    } else {
+        console.log(req.body)
+        readFile('./tasks.json')
+        .then(tasks =>{
+            let index
+            if(tasks.length === 0)
+            {
+                index = 0
+            } else {
+                index = tasks[tasks.length-1].id + 1;
+            }
+            
+            const newTask = {
+                "id" : index,
+                "task" : req.body.task
+            }
+            if (isValidString(req.body.task)) {
+                tasks.push(newTask)
+                data = JSON.stringify(tasks, null, 2)
+                writeFile('./tasks.json', data)
+            } else {
+                console.log("Refused empty string or value that was not a string.")
+            }
+            res.redirect('/')
+        })    
+    }
 })
 
 app.listen(3001, () => {
