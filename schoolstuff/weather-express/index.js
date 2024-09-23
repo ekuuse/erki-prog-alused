75fc97703c03d059d8227bcf22a1db030ca30fb9
ekuuse <erki.kuuse@voco.ee>
2024-09-23 10:26:10 +0300

    refactored code dublicate with promice for data

--- schoolstuff/weather-express/index.js
+++ schoolstuff/weather-express/index.js
@@ -5,27 +5,47 @@ const fetch = require('node-fetch')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
 
 const key = "a20f9f90b5af6dd45fa41c9373a929b2"
-let city = "Tartu"
 
-app.get('/', function(req, res) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
-    .then((response) => {
-        return response.json()
-    })
-    .then((data) => {
-        let description = data.weather[0].description
-        let city = data.name
-        let temp = Math.round(parseFloat(data.main.temp)-273.15)
-        console.log(description)
-        console.log(city)
-        console.log(temp)
-        res.render('index',{
-            description: description,
-            city: city,
-            temp: temp
+const getWeatherDataPromise = (url) => {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+        .then(response => {
+            return response.json()
+        })
+        .then((data) => {
+            let description = data.weather[0].description
+            let city = data.name
+            let temp = Math.round(parseFloat(data.main.temp)-273.15)
+            let result = {
+                description: description,
+                city: city,
+                temp: temp
+            }
+            resolve(result)
+        })
+        .catch(error => {
+            reject(error)
         })
     })
+}
+
+app.all('/', function(req, res) {
+    let city
+    if(req.method == 'GET'){
+        city = 'Tartu'
+    }
+    if (req.method == 'POST'){
+        city = req.body.cityname
+    }
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+    getWeatherDataPromise(url)
+    .then(data => {
+        res.render('index', data)
+    })
 })
+
 app.listen(3000)
