d63379fd7b4813b6a67501d49a9575a2c4e519e7
ekuuse <erki.kuuse@voco.ee>
2024-10-21 09:33:25 +0300

    Read todos from file and write todos into file

--- schoolstuff/todo-oop/controllers/todos.js
+++ schoolstuff/todo-oop/controllers/todos.js
@@ -1,22 +1,71 @@
 import { Todo } from '../models/todo.js'
+import { fileManager } from './files.js'
 
 class TodoController {
     constructor(){
         this.TODOS = []
     }
 
-    createTodo(req,res){
+    async createTodo(req,res){
         const task = req.body.task
         const newTodo = new Todo(Math.random().toString(), task)
         this.TODOS.push(newTodo)
+        await fileManager.writeFile('./data/todos.json', this.TODOS)
         res.json({
             message: 'created new todo object',
             newTask: newTodo
         })
     }
 
+    async initTodos(){
+        const todosData = await fileManager.readFile('./data/todos.json')
+        if (todosData !== null){
+            this.TODOS = todosData
+        } else {
+            this.TODOS = []
+        }
+    }
+
     getTodos(req, res){
         res.json({tasks: this.TODOS})
+    }
+
+    updateTodo(req,res){
+        const todoId = req.params.id
+        const updatedTask = req.body.task
+        const todoIndex = this.TODOS.findIndex((todo) => todo.id === todoId)
+
+        if (todoIndex < 0) {
+            throw new Error('Could not find todo!')
+            res.json({
+                message: 'Could not find todo with such index'
+            })
+        }
+
+        this.TODOS[todoIndex] = new Todo(this.TODOS[todoIndex].id, updatedTask)
+        res.json({
+            message: 'Updated todo',
+            updatedTask: this.TODOS[todoIndex]
+        })
+    }
+
+    deleteTodo(req,res){
+        const todoId = req.params.id
+        const updatedTask = req.body.task
+        const todoIndex = this.TODOS.findIndex((todo) => todo.id === todoId)
+
+        if (todoIndex < 0) {
+            throw new Error('Could not find todo!')
+            res.json({
+                message: 'Could not find todo with such index'
+            })
+        }
+
+        this.TODOS.splice(todoIndex, 1)
+
+        res.json({
+            message: 'Deleted todo'
+        })
     }
 }
 
--- schoolstuff/todo-oop/routes/todos.js
+++ schoolstuff/todo-oop/routes/todos.js
@@ -4,5 +4,7 @@ import { todoController } from '../controllers/todos.js'
 const router = Router()
 router.post('/new-todo', (req,res) => todoController.createTodo(req,res))
 router.get('/', (req,res) => todoController.getTodos(req,res))
+router.patch('/:id', (req,res) => todoController.updateTodo(req,res))
+router.delete('/:id', (req,res) => todoController.deleteTodo(req,res))
 
 export default router
