1b2d40d3ad5b2c183facf48ee7aef20d1d5bf537
ekuuse <erki.kuuse@voco.ee>
2024-11-19 09:35:30 +0200

    Create new Todo from user data

--- schoolstuff/typescript-oop/src/app.ts
+++ schoolstuff/typescript-oop/src/app.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response, NextFunction } from 'express'
+import { json } from 'body-parser'
+
 import todoRoutes from './routes/todos'
-import { error } from 'console'
 
 const app = express()
 
+app.use(json())
 app.use('/todos', todoRoutes)
 
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
